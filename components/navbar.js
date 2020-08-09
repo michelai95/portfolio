90cb8f1372c5259fff332e0a42a42ea9b0ab914d
michela iacobucci <michelaiacobucci@MacBook-Air.local>
2020-08-09 10:39:41 -0700

    build

---
+++ components/navbar.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import React, { useEffect } from 'react'
+
+export default function Navbar() {
+    useEffect(() => {
+        document.getElementById('home').style.color = 'green';
+    }, [])
+    return (
+        <div>
+            <Head>
+                <title>Michela's Portfolio</title>
+            </Head>
+            <navbar>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>
+                        <Link href="">
+                            <a>Projects</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                            <a>Contact</a>
+                        </Link>
+                    </li>
+                </ul>
+            </navbar>
+        </div>
+    )
+} 
---
+++ portfolio/pages/about.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/navbar'
+
+export default function About() {
+    return (
+        <div>
+            <Navbar />
+            <div>Hi, my name is Michela!</div>
+        </div>
+    )
+}
