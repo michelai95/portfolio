e7a5318a39cac5ae654bf190c3a09e4c775ad0f2
michela iacobucci <michelaiacobucci@MacBook-Air.local>
2020-08-09 11:11:11 -0700

    build1

---
+++ blog_post/post.js
@@ -0,0 +1,11 @@
+export default function Post() {
+    return (
+        <h1 id="hello">This is a new blog post</h1>
+        <style jsx>
+            {`
+            #hello {
+                color: green;
+            }`}
+        </style>
+    )
+}
