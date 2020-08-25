04615568ca5cf40a971b99a3f51fb9392be4de6b
michela iacobucci <miacobucci17@gmail.com>
2020-08-25 09:38:30 -0700

    styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e8e1b9e93eae32f35b37";
+/******/ 	var hotCurrentHash = "1f56357f226252cb130e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
